5ad8190de8c9cf9c474419bb1e92f26271000abe
stanislas jouven <stanislasjouven@gmail.com>
2019-11-09 18:30:19 +0100

    name error

--- source_loc.py
+++ source_loc.py
@@ -9,7 +9,7 @@ from multiprocessing import Pool
 from termcolor import colored
 
 ### Compute a batch in parallel
-def glad_naive(graph, obs_time, distribution) :
+def glad_modified(graph, obs_time, distribution) :
     mu = distribution.mean()
     sigma = distribution.std()
     obs = np.array(list(obs_time.keys()))
